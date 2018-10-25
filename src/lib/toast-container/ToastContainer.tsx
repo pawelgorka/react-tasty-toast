87746c99221a86d48ddc6340f2da2732e98a7358
pawel-gorka <git@pawelgorka.com>
2018-10-25 22:11:08 +0200

    feat(toast-container): show toast component on show toast event
    toast container listens on container show event (listening starts inside component did mount)

--- src/lib/toast-container/ToastContainer.tsx
+++ src/lib/toast-container/ToastContainer.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import withEventManager, { IWithEventManagerInjectedProps } from '../hoc/withEventManager'
 import { EventType, IShowToastPayload } from '../events/events'
+import { ToastFactoryFunction } from '../toast.model'
 
 export interface IToastContainerProps extends IWithEventManagerInjectedProps {}
 
 interface IToastContainerState {
   toastIds: number[]
+  toasts: { [toastId: number]: ToastFactoryFunction }
 }
 
 class ToastContainer extends React.Component<IToastContainerProps, IToastContainerState> {
@@ -13,30 +15,41 @@ class ToastContainer extends React.Component<IToastContainerProps, IToastContain
     super(props)
 
     this.state = {
-      toastIds: []
+      toastIds: [],
+      toasts: {}
     }
   }
 
   componentDidMount() {
-    this.props.eventManager
-      .emit(EventType.ContainerDidMount, undefined)
+    this.props.eventManager.on(EventType.Show, this.onShowToast.bind(this))
+    this.props.eventManager.emit(EventType.ContainerDidMount, undefined)
   }
 
   componentWillUnmount() {
-    this.props.eventManager
-      .emit(EventType.ContainerDidUnmount, undefined);
+    this.props.eventManager.emit(EventType.ContainerDidUnmount, undefined)
   }
 
-  showToast(showEventPayload: IShowToastPayload) {
+  onShowToast(showEventPayload: IShowToastPayload) {
     const { content, options } = showEventPayload
 
     const toastId = options.toastId
 
-    this.setState(state => ({ ...state, toastIds: [...state.toastIds, toastId] }))
+    this.setState(state => ({
+      ...state,
+      toastIds: [...state.toastIds, toastId],
+      toasts: { ...state.toasts, [toastId]: content }
+    }))
   }
 
   render() {
-    return <div />
+    const { toastIds, toasts } = this.state
+    return (
+      <div>
+        {toastIds.map(toastId => {
+          return <React.Fragment key={toastId}>{toasts[toastId]()}</React.Fragment>
+        })}
+      </div>
+    )
   }
 }
 
--- test/lib/toast-container/ToastContainer.test.tsx
+++ test/lib/toast-container/ToastContainer.test.tsx
@@ -3,6 +3,7 @@ import { mount } from 'enzyme'
 import { ToastContainer } from '../../../src/lib/toast-container/ToastContainer'
 import { IEventManager } from '../../../src/lib/event-manager/event-manager'
 import withEventManager from '../../../src/lib/hoc/withEventManager'
+import { EventType, IShowToastPayload } from '../../../src/lib/events/events'
 
 const getToastContainerComponent = (eventManager: IEventManager) => {
   return withEventManager(ToastContainer, eventManager)
@@ -10,19 +11,44 @@ const getToastContainerComponent = (eventManager: IEventManager) => {
 
 describe('A <ToastContainer />', () => {
   let EventManagerMock: jest.Mock<IEventManager>
+  let eventManagerMock: IEventManager
+  let onMountCallback: () => void
+  let onUnmountCallback: () => void
+  let onShowToastCallback: (showToastPayload: IShowToastPayload) => void
 
   beforeEach(() => {
     EventManagerMock = jest.fn<IEventManager>(() => ({
       emit: jest.fn(),
-      on: jest.fn(),
+      on: jest.fn().mockImplementation((eventType: EventType, callback) => {
+        if (eventType === EventType.ContainerDidMount) {
+          onMountCallback = callback
+        } else if (eventType === EventType.ContainerDidUnmount) {
+          onUnmountCallback = callback
+        } else if (eventType === EventType.Show) {
+          onShowToastCallback = callback
+        }
+      }),
       off: jest.fn()
     }))
+
+    eventManagerMock = new EventManagerMock()
   })
 
   it('should emit ToastContainerMounted event on mount', () => {
-    const eventManagerMock = new EventManagerMock()
     const ToastContainerComponent = getToastContainerComponent(eventManagerMock)
     mount(<ToastContainerComponent />)
     expect(eventManagerMock.emit).toHaveBeenCalledTimes(1)
+  })
+
+  it('should render Toast Component on show toast event', () => {
+    const ToastContainerComponent = getToastContainerComponent(eventManagerMock)
+    const wrapper = mount(<ToastContainerComponent />)
+
+    onShowToastCallback({
+      content: () => <span>lorem 1</span>,
+      options: { autoClose: false, toastId: 1 }
+    })
+
+    expect(wrapper.text()).toContain('lorem 1')
   })
 })
